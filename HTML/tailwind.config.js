d69e2d4469e521c50a59fa62b6e07c16dd310ba5
Jorge Díez Forcada <132222291+Jorgete03@users.noreply.github.com>
2025-01-19 22:33:58 +0100

    Add files via upload

---
+++ HTML/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./citas-anteriores.html","./resumen-cita-trauma.html","./resumen-cita-rayos.html","./recetas.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
